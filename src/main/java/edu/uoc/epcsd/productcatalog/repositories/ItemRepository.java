7c3f18d4eeddc4dcc773a05a0b8f648be0376bd5
Vicenç Font Sagristà <vfont@uoc.edu>
2023-11-25 08:14:33 +0100

    Update ItemRepository.java

--- src/main/java/edu/uoc/epcsd/productcatalog/repositories/ItemRepository.java
+++ src/main/java/edu/uoc/epcsd/productcatalog/repositories/ItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, String> {
 
     Optional<Item> findBySerialNumber(String serialNumber);
 
